6d68279f7fcf1307a5234a9bd42af874b06c45c5
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-11-21 14:55:49 -0500

    Ball Physics - Just started pt.20

---
+++ Pong/Assets/Scripts/Launch.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour 
+{
+    public float PreLaunchDelay;
+    public float Force;
+}
