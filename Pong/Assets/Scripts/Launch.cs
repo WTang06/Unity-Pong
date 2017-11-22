dee91beed4e00a34fc43e44481dae93b5484fc38
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-11-22 14:48:54 -0500

    Unity Pong pt. 21
    AT 7:32 in the video
    Added sound to both paddles
    Need to be tagged

--- Pong/Assets/Scripts/Launch.cs
+++ Pong/Assets/Scripts/Launch.cs
@@ -6,4 +6,32 @@ public class Launch : MonoBehaviour
 {
     public float PreLaunchDelay;
     public float Force;
+    public Rigidbody2D RigidBody;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(PreLaunchDelay);
+
+        Vector2 direction = Vector2.zero;
+
+        int rndInt = Random.Range(0, 4);
+
+        switch (rndInt)
+        {
+            case 0:
+                direction = new Vector2(1f, 0.5f);
+                break;
+            case 1:
+                direction = new Vector2(1f, -0.5f);
+                break;
+            case 2:
+                direction = new Vector2(-1f, -0.5f);
+                break;
+            case 3:
+                direction = new Vector2(-1f, 0.5f);
+                break;
+        }
+
+        RigidBody.AddForce(direction * Force);
+    }
 }
