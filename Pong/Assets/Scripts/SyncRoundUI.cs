44002ed696817f60405244bb1290cb059683fa43
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-11-24 14:46:34 -0500

    Got up to pt.26
    Need to redo pt. 26
    SyncRoundUI script not working

---
+++ Pong/Assets/Scripts/SyncRoundUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SyncRoundUI : MonoBehaviour 
+{
+    public DataManager DataManager;
+    public Text TextToUpdate;
+
+    public enum Player { P1, P2 }
+    public Player PlayerScore;
+
+    void Awake()
+    {
+        DataManager = GameObject.FindWithTag("DataManager").GetComponent<DataManager>();
+    }
+
+    void Update()
+    {
+        if (PlayerScore == Player.P1)
+        {
+            TextToUpdate.text = DataManager.P1Score;
+        }
+        else if (PlayerScore == Player.P2)
+        {
+            TextToUpdate.text = DataManager.P2Score;
+        }
+        else
+        {
+            throw new UnityException("Invalid Player!");
+        }
+    }
+}
