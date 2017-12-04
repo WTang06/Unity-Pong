99670f9d499a34ee898d866f3b0fd04bd74e2bc5
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-12-04 13:52:41 -0500

    IDK what this stuff is

---
+++ Pong/Assets/Scripts/RestartOrQuit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOrQuit : MonoBehaviour 
+{
+    public KeyCode RestartKey;
+    public KeyCode QuitKey;
+    public DataManager DataManager;
+
+    void Awake()
+    {
+        DataManager = GameObject.FindWithTag("DataManager").GetComponent<DataManager>();
+    }
+    //This gets the DataManager for the code.
+
+    void Update()
+    {
+        if (Input.GetKeyDown(RestartKey))
+        {
+            DataManager.P1Score = 0;
+            DataManager.P2Score = 0;
+            DataManager.WinnerMessage = "";
+            SceneManager.LoadScene("Title");
+        }
+        else if (Input.GetKeyDown(QuitKey))
+        {
+            Application.Quit();
+        }
+    }
+}
