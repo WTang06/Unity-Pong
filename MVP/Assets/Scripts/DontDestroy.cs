b9a8514e0e873ebb988f47d091c82d072ec68ff2
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-12-05 14:46:34 -0500

    Saved the DontDestroy script

--- MVP/Assets/Scripts/DontDestroy.cs
+++ MVP/Assets/Scripts/DontDestroy.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroy : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class DontDestroy : MonoBehaviour 
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 }
