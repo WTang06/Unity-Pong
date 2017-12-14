98234bacaab034aaeb3c6fd4ba99854bebec7398
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-12-14 14:59:46 -0500

    Title and Load Scene
    Done! Need to Make map loader

---
+++ MVP/Assets/Scripts/LoadSceneOnKeyDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnKeyDown : MonoBehaviour 
+{
+    public string SceneToLoad;
+    public float PreKeyDelay = 1f;
+    public float PostKeyDelay = 1f;
+    public KeyCode Key = KeyCode.Return;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(PreKeyDelay);
+
+        while (!Input.GetKeyDown(Key))
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(PostKeyDelay);
+
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+}
