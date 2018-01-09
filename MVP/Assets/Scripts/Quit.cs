c6a045c3aaf4b833cf64664ac053443a2adcb46c
WTang06 <33232639+WTang06@users.noreply.github.com>
2018-01-09 14:57:35 -0500

    Fix Animation
    Make animation less choppy + make camera follow player

---
+++ MVP/Assets/Scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour 
+{
+	public KeyCode QuitKey;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(QuitKey)) 
+		{
+			Application.Quit ();
+		}
+	}
+		
+}
